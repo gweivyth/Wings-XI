c5b587d8e064645f4b3d5f47e80df1391e5787fa
gweivyth <37130689+gweivyth@users.noreply.github.com>
2021-02-02 09:19:45 -0500

    Add files via upload

---
+++ third_eye.lua
@@ -0,0 +1,24 @@
+-----------------------------------
+-- Ability: Third Eye
+-- Anticipates and dodges the next attack directed at you.
+-- Obtained: Samurai Level 15
+-- Recast Time: 1:00
+-- Duration: 0:30
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+-----------------------------------
+
+function onAbilityCheck(player, target, ability)
+    if (player:hasStatusEffect(tpz.effect.SEIGAN)) then
+        ability:setRecast(ability:getRecast()/2)
+    end
+    return 0, 0
+end
+
+function onUseAbility(player, target, ability)
+	if (player:hasStatusEffect(tpz.effect.COPY_IMAGE)) then
+    effect:setMsg(tpz.msg.basic.JA_NO_EFFECT) else --Returns "no effect" message when Copy Image is active when Third Eye is used.
+    player:addStatusEffect(tpz.effect.THIRD_EYE, 0, 0, 30) --power keeps track of procs
+end
+end
